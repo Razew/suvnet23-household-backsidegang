7212c99ff6d9cf168a95c48e802d63e4127a8ce3
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2024-10-10 12:14:46 +0200

    Deleted old files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,11 @@
-import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
 import globals from 'globals';
+import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { languageOptions: { globals: globals.node } },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-    },
-  },
 ];
