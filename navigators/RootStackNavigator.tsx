65224581f6d09ed11896d28ca4cf98dbb44370c4
Markus <markus147@live.se>
2024-10-11 12:22:48 +0200

    Add root stack navigator

---
+++ navigators/RootStackNavigator.tsx
@@ -0,0 +1,39 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoadingScreen from '../screens/LoadingScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import HomeStackNavigator from './HomeStackNavigator';
+
+export type RootStackParamList = {
+  Loading: undefined;
+  Login: undefined;
+  Register: undefined;
+  HomeNavigator: undefined;
+};
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootStackNavigator() {
+  return (
+    <RootStack.Navigator initialRouteName="Login">
+      <RootStack.Screen
+        name="Loading"
+        component={LoadingScreen}
+        options={{ headerShown: false }}
+      />
+      <RootStack.Screen
+        name="Login"
+        component={LoginScreen}
+      />
+      <RootStack.Screen
+        name="Register"
+        component={RegisterScreen}
+      />
+      <RootStack.Screen
+        name="HomeNavigator"
+        component={HomeStackNavigator}
+        options={{ headerShown: false }}
+      />
+    </RootStack.Navigator>
+  );
+}
