390573d7fb384983d284d1d69705b9eeecb4579f
Markus <Markus147@live.se>
2024-10-11 15:54:59 +0200

    Add statistics screen to navigator

--- navigators/HouseholdTabNavigator.tsx
+++ navigators/HouseholdTabNavigator.tsx
@@ -1,14 +1,26 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import HouseholdScreen from '../screens/HouseholdScreen';
+import StatisticsScreen from '../screens/StatisticsScreen';
 
-const Tab = createMaterialTopTabNavigator();
+export type HouseholdTabParamList = {
+  Household: undefined;
+  Statistics: undefined;
+  // Schedule: undefined;
+  // Admin: undefined; // This is just an example
+};
+
+const Tab = createMaterialTopTabNavigator<HouseholdTabParamList>();
 
 export default function HouseholdTabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator initialRouteName="Household">
       <Tab.Screen
         name="Household"
         component={HouseholdScreen}
+      />
+      <Tab.Screen
+        name="Statistics"
+        component={StatisticsScreen}
       />
     </Tab.Navigator>
   );
