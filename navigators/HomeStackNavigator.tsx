701e144e283c2c8c04b259a44e8e30b021f46902
Markus <Markus147@live.se>
2024-10-11 15:54:00 +0200

    Add nested household navigator

--- navigators/HomeStackNavigator.tsx
+++ navigators/HomeStackNavigator.tsx
@@ -2,11 +2,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CreateHouseholdScreen from '../screens/CreateHouseholdScreen';
 import HomeScreen from '../screens/HomeScreen';
 import JoinHouseholdScreen from '../screens/JoinHouseholdScreen';
+import HouseholdTabNavigator from './HouseholdTabNavigator';
 
 export type HomeStackParamList = {
   Home: undefined;
   JoinHousehold: undefined;
   CreateHousehold: undefined;
+  HouseholdNavigator: undefined;
 };
 
 const HomeStack = createNativeStackNavigator<HomeStackParamList>();
@@ -30,6 +32,11 @@ export default function HomeStackNavigator() {
         name="CreateHousehold"
         component={CreateHouseholdScreen}
         options={{ title: 'Create Household' }}
+      />
+      <HomeStack.Screen
+        name="HouseholdNavigator"
+        component={HouseholdTabNavigator}
+        options={{ title: 'Household' }}
       />
     </HomeStack.Navigator>
   );
