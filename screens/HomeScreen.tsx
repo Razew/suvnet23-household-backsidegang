934e0450fc33e835cf68664ecb813c2d67d3b764
Markus <Markus147@live.se>
2024-10-11 15:53:17 +0200

    Add temp buttons for navigation

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,29 @@
-import { Text, View } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Button, Text, View } from 'react-native';
+import { HomeStackParamList } from '../navigators/HomeStackNavigator';
 import { container, large } from '../themes/styles';
 
-export default function HomeScreen() {
+type Props = NativeStackScreenProps<HomeStackParamList, 'Home'>;
+
+export default function HomeScreen({ navigation }: Props) {
   return (
     <View style={container}>
       <Text style={large}>Home screen</Text>
+      <View>
+        <Text>Davidsson Household</Text>
+        <Button
+          title="Enter"
+          onPress={() => navigation.replace('HouseholdNavigator')}
+        />
+      </View>
+      <Button
+        title="Join Household"
+        onPress={() => navigation.navigate('JoinHousehold')}
+      />
+      <Button
+        title="Create Household"
+        onPress={() => navigation.navigate('CreateHousehold')}
+      />
     </View>
   );
 }
