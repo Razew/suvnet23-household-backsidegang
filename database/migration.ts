5b7c5c77ec2fbf2a5f5a9380d95b6911f2e80871
88jonhan <88jonhan@skola.boras.se>
2024-10-11 13:17:04 +0200

    added query for creating tables

--- database/migration.ts
+++ database/migration.ts
@@ -23,6 +23,63 @@ async function dropTables() {
   }
 }
 
+async function createTables() {
+  console.log('Running createTables()...');
+  const createTablesQuery = `
+  CREATE TABLE IF NOT EXISTS household (
+      id SERIAL PRIMARY KEY,
+      name TEXT NOT NULL,
+      code TEXT NOT NULL
+  );
+  CREATE TABLE IF NOT EXISTS chore (
+      id SERIAL PRIMARY KEY,
+      name TEXT NOT NULL,
+      description TEXT,
+      household_id serial REFERENCES household(id),
+      is_active BOOLEAN DEFAULT TRUE,
+      frequency INTEGER,
+      is_archived BOOLEAN DEFAULT FALSE,
+      weight INTEGER NOT NULL CHECK (weight IN (1, 2, 4, 6, 8))
+  );
+  CREATE TABLE IF NOT EXISTS account (
+      id SERIAL PRIMARY KEY,
+      user_name TEXT NOT NULL,
+      hashed_password TEXT NOT NULL
+  );
+  CREATE TABLE IF NOT EXISTS avatar (
+      id SERIAL PRIMARY KEY,
+      name TEXT NOT NULL,
+      image bytea NOT NULL,
+      colour_code TEXT NOT NULL
+  );
+  CREATE TABLE IF NOT EXISTS chore_to_user (
+      account_id INTEGER REFERENCES account(id),
+      chore_id INTEGER REFERENCES chore(id),
+      is_completed BOOLEAN DEFAULT FALSE,
+      due_date TIMESTAMP,
+      done_date TIMESTAMP
+  );
+  CREATE TABLE IF NOT EXISTS user_to_household (
+      account_id INTEGER REFERENCES account(id),
+      household_id INTEGER REFERENCES household(id),
+      avatar_id INTEGER REFERENCES avatar(id),
+      nickname TEXT,
+      isActive BOOLEAN DEFAULT TRUE,
+      isAdmin BOOLEAN DEFAULT FALSE
+  );
+  `;
+
+  const { data, error } = await supabase.rpc('execute_sql', {
+    sql: createTablesQuery,
+  });
+  if (error) {
+    console.error('Error while running function createTables():', error);
+  } else {
+    console.log('"createTables() result:', data);
+  }
+  await grantPermissionsToTables();
+}
+
 async function grantPermissionsToTables() {
   console.log('Running grantPermissionsToTables()...');
   const grantPermissionsToTablesQuery = `
@@ -75,5 +132,7 @@ export function migrate() {
     await signUpOrLogIn(supabaseUsername, supabasePassword);
     console.log('dropping tables...');
     await dropTables();
+    console.log('creating tables...');
+    await createTables();
   })();
 }
