979142ab48b0173ae61d9c20332608c211fdd1d9
Markus <Markus147@live.se>
2024-10-11 16:47:20 +0200

    Add temp 'exit' buttons in header for navigating back

--- navigators/HomeStackNavigator.tsx
+++ navigators/HomeStackNavigator.tsx
@@ -1,4 +1,6 @@
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Pressable, StyleSheet, Text } from 'react-native';
 import CreateHouseholdScreen from '../screens/CreateHouseholdScreen';
 import HomeScreen from '../screens/HomeScreen';
 import JoinHouseholdScreen from '../screens/JoinHouseholdScreen';
@@ -13,11 +15,28 @@ export type HomeStackParamList = {
 
 const HomeStack = createNativeStackNavigator<HomeStackParamList>();
 
+// both headerRight pressables are just there to make
+// navigating back easier during development
 export default function HomeStackNavigator() {
   return (
     <HomeStack.Navigator
       initialRouteName="Home"
-      screenOptions={{ headerBackTitle: 'Back' }}
+      screenOptions={({ navigation }) => ({
+        headerRight: () => (
+          <Pressable
+            style={s.tempExit}
+            onPress={() => navigation.replace('Loading')}
+          >
+            <Text style={s.tempText}>Start</Text>
+            <MaterialIcons
+              name="exit-to-app"
+              size={24}
+              color="#D32F2F"
+            />
+          </Pressable>
+        ),
+        headerBackTitle: 'Back',
+      })}
     >
       <HomeStack.Screen
         name="Home"
@@ -36,8 +55,36 @@ export default function HomeStackNavigator() {
       <HomeStack.Screen
         name="HouseholdNavigator"
         component={HouseholdTabNavigator}
-        options={{ title: 'Household' }}
+        options={({ navigation }) => ({
+          title: 'Household',
+          headerRight: () => (
+            <Pressable
+              style={s.tempExit}
+              onPress={() => navigation.replace('Home')}
+            >
+              <Text style={s.tempText}>Home</Text>
+              <MaterialIcons
+                name="exit-to-app"
+                size={24}
+                color="#D32F2F"
+              />
+            </Pressable>
+          ),
+        })}
       />
     </HomeStack.Navigator>
   );
 }
+
+// TEMPORARY
+const s = StyleSheet.create({
+  tempExit: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 3,
+  },
+  tempText: {
+    color: '#B0B0B0',
+    fontWeight: '500',
+  },
+});
