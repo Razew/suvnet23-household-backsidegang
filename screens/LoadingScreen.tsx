afed29ebd3ae584337fb2976dc3d34d8ecb99ffd
Markus <markus147@live.se>
2024-10-11 11:55:57 +0200

    Add temp go to login button

--- screens/LoadingScreen.tsx
+++ screens/LoadingScreen.tsx
@@ -1,10 +1,18 @@
-import { Text, View } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Button, Text, View } from 'react-native';
+import { RootStackParamList } from '../navigators/RootStackNavigator';
 import { container, large } from '../themes/styles';
 
-export default function LoadingScreen() {
+type Props = NativeStackScreenProps<RootStackParamList, 'Loading'>;
+
+export default function LoadingScreen({ navigation }: Props) {
   return (
     <View style={container}>
       <Text style={large}>Loading screen</Text>
+      <Button
+        title="Go to Login"
+        onPress={() => navigation.replace('Login')}
+      />
     </View>
   );
 }
