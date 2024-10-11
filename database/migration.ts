c5e2bbabbb633f0a0d1ff3b15b408e9dac7625bb
88jonhan <88jonhan@skola.boras.se>
2024-10-11 13:17:27 +0200

    added temp query for mocking data in db for testing

--- database/migration.ts
+++ database/migration.ts
@@ -118,6 +118,39 @@ async function signUpOrLogIn(email: string, password: string) {
   console.log('User logged in:', user);
 }
 
+export async function addMockData() {
+  const addMockDataQuery = `
+  INSERT INTO household (name, code) VALUES ('Household 1', '1234');
+  INSERT INTO household (name, code) VALUES ('Household 2', '5678');
+  INSERT INTO account (user_name, hashed_password) VALUES ('Alice', '1234');
+  INSERT INTO account (user_name, hashed_password) VALUES ('Bob', '5678');
+  INSERT INTO avatar (name, image, colour_code) VALUES ('Avatar 1', 'image1', 'red');
+  INSERT INTO avatar (name, image, colour_code) VALUES ('Avatar 2', 'image2', 'blue');
+  INSERT INTO chore (name, description, household_id, is_active, frequency, is_archived, weight) VALUES ('Chore 1', 'Description 1', 1, TRUE, 1, FALSE, 1);
+  INSERT INTO chore (name, description, household_id, is_active, frequency, is_archived, weight) VALUES ('Chore 2', 'Description 2', 1, TRUE, 2, FALSE, 2);
+  INSERT INTO chore (name, description, household_id, is_active, frequency, is_archived, weight) VALUES ('Chore 3', 'Description 3', 1, TRUE, 3, FALSE, 4);
+  INSERT INTO chore (name, description, household_id, is_active, frequency, is_archived, weight) VALUES ('Chore 4', 'Description 4', 1, TRUE, 4, FALSE, 6);
+  INSERT INTO chore (name, description, household_id, is_active, frequency, is_archived, weight) VALUES ('Chore 5', 'Description 5', 1, TRUE, 5, FALSE, 8);
+  INSERT INTO chore_to_user (account_id, chore_id, is_completed, due_date, done_date) VALUES (1, 1, FALSE, '2021-01-01', NULL);
+  INSERT INTO chore_to_user (account_id, chore_id, is_completed, due_date, done_date) VALUES (1, 2, FALSE, '2021-01-02', NULL);
+  INSERT INTO chore_to_user (account_id, chore_id, is_completed, due_date, done_date) VALUES (1, 3, FALSE, '2021-01-03', NULL);
+  INSERT INTO chore_to_user (account_id, chore_id, is_completed, due_date, done_date) VALUES (1, 4, FALSE, '2021-01-04', NULL);
+  INSERT INTO chore_to_user (account_id, chore_id, is_completed, due_date, done_date) VALUES (1, 5, FALSE, '2021-01-05', NULL);
+  INSERT INTO user_to_household (account_id, household_id, avatar_id, nickname, isActive, isAdmin) VALUES (1, 1, 1, 'Alice', TRUE, TRUE);
+  INSERT INTO user_to_household (account_id, household_id, avatar_id, nickname, isActive, isAdmin) VALUES (1, 2, 2, 'Alice', TRUE, TRUE);
+  INSERT INTO user_to_household (account_id, household_id, avatar_id, nickname, isActive, isAdmin) VALUES (2, 1, 1, 'Bob', TRUE, FALSE);
+  INSERT INTO user_to_household (account_id, household_id, avatar_id, nickname, isActive, isAdmin) VALUES (2, 2, 2, 'Bob', TRUE, FALSE);
+  `;
+  const { data, error } = await supabase.rpc('execute_sql', {
+    sql: addMockDataQuery,
+  });
+  if (error) {
+    console.error('Error while running function addMockData():', error);
+  } else {
+    console.log('"addMockData() result:', data);
+  }
+}
+
 export function migrate() {
   console.log('Running migrate()...');
   (async () => {
@@ -134,5 +167,7 @@ export function migrate() {
     await dropTables();
     console.log('creating tables...');
     await createTables();
+    console.log('adding mock data...');
+    await addMockData();
   })();
 }
