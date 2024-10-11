ef4783b3a4d651665c9fc9795254eadfc3090ff3
Markus <Markus147@live.se>
2024-10-11 15:29:58 +0200

    Add navigation buttons for testing

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,10 +1,22 @@
-import { Text, View } from 'react-native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { Button, Text, View } from 'react-native';
+import { RootStackParamList } from '../navigators/RootStackNavigator';
 import { container, large } from '../themes/styles';
 
-export default function LoginScreen() {
+type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
+
+export default function LoginScreen({ navigation }: Props) {
   return (
     <View style={container}>
       <Text style={large}>Login screen</Text>
+      <Button
+        title="Log in"
+        onPress={() => navigation.replace('HomeNavigator')}
+      />
+      <Button
+        title="Register"
+        onPress={() => navigation.navigate('Register')}
+      />
     </View>
   );
 }
