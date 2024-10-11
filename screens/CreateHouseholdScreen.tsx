1bdb55488fe500b663aa2647fb78012aff2bfc17
Markus <markus147@live.se>
2024-10-11 11:32:10 +0200

    Add join and create household screens

---
+++ screens/CreateHouseholdScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import { container, large } from '../themes/styles';
+
+export default function CreateHouseholdScreen() {
+  return (
+    <View style={container}>
+      <Text style={large}>Create household screen</Text>
+    </View>
+  );
+}
