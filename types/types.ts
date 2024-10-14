85642878c3b9f4dec02403eab23abd0fde9a57fd
88jonhan <88jonhan@skola.boras.se>
2024-10-14 13:27:22 +0200

    added properties to types to match db tables

--- types/types.ts
+++ types/types.ts
@@ -1,12 +1,39 @@
 export type Account = {
   id: number;
+  user_name: string;
+  hashed_password: string;
 };
 export type Chore = {
   id: number;
+  name: string;
+  description: string;
+  household: Household;
+  is_active: boolean;
+  frequency: number;
+  is_archived: boolean;
+  voice_recording: string;
+  image: string;
+  weight: 1 | 2 | 4 | 6 | 8;
 };
 export type Household = {
   id: number;
+  name: string;
+  code: string;
+  chores: Chore[];
 };
 export type Avatar = {
   id: number;
+  name: string;
+  image: string;
+  colour_code: string;
+};
+
+export type HouseholdUserProfile = {
+  id: number;
+  nickname: string;
+  account: Account;
+  household: Household;
+  avatar: Avatar;
+  is_active: boolean;
+  is_admin: boolean;
 };
