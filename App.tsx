81b88fa4a6447a0f31c238c74968e9ab80e0ce74
88jonhan <88jonhan@skola.boras.se>
2024-10-15 16:08:33 +0200

    rendering avatars for testing

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import RootStackNavigator from './navigators/RootStackNavigator';
-
+import { mockedAvatars } from './data/mocked';
+import { Text, View } from 'react-native';
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <RootStackNavigator />
+      <View style={{ width: '100%', height: '100%', marginTop: 40 }}>
+        {mockedAvatars.map((avatar) => (
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={{ fontSize: 48 }}>{avatar.image}</Text>
+            <View
+              style={{
+                backgroundColor: avatar.colour_code,
+                width: 100,
+                height: 100,
+              }}
+            />
+          </View>
+        ))}
+        {/* <RootStackNavigator /> */}
+      </View>
     </NavigationContainer>
   );
 }
