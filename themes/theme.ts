c27618b9daeaffc4e91b452a1d6c6149e5b2f708
Markus <Markus147@live.se>
2024-10-16 01:26:27 +0200

    Combine light and dark themes

--- themes/theme.ts
+++ themes/theme.ts
@@ -1,1 +1,36 @@
-// TODO: Add dark and light mode here
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationLightTheme,
+  Theme,
+} from '@react-navigation/native';
+import {
+  adaptNavigationTheme,
+  MD3DarkTheme,
+  MD3LightTheme,
+} from 'react-native-paper';
+import { ThemeProp } from 'react-native-paper/lib/typescript/types';
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationLightTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+export type AppTheme = ThemeProp & Theme;
+
+export const combinedLightTheme: AppTheme = {
+  ...MD3LightTheme,
+  ...LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    ...LightTheme.colors,
+  },
+};
+
+export const combinedDarkTheme: AppTheme = {
+  ...MD3DarkTheme,
+  ...DarkTheme,
+  colors: {
+    ...MD3DarkTheme.colors,
+    ...DarkTheme.colors,
+  },
+};
