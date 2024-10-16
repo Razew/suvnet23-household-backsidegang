74fdbbbb423119c47382aab49356913640e28632
Alex <alexjoelaid97@gmail.com>
2024-10-16 10:17:08 +0200

    recommit since last "bugged" up

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,27 +1,64 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
-import { RootStackParamList } from '../navigators/RootStackNavigator';
-import { container, large } from '../themes/styles';
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import hushallet_logo from '../assets/logo/hushallet_logo.png';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
-
-export default function LoginScreen({ navigation }: Props) {
+const SignIn = () => {
+  const navigation = useNavigation();
   return (
-    <View style={container}>
-      <Text style={large}>Login screen</Text>
-      <Button
-        mode="elevated"
-        onPress={() => navigation.replace('HomeNavigator')}
-      >
-        Log in
-      </Button>
-      <Button
-        mode="elevated"
-        onPress={() => navigation.navigate('Register')}
-      >
-        Register
-      </Button>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View
+          style={{ flex: 1, alignItems: 'center', backgroundColor: 'white' }}
+        >
+          <Image
+            source={hushallet_logo}
+            resizeMode="contain"
+            style={{ width: '100%' }}
+          />
+          <View style={{ width: '80%', gap: 5 }}>
+            <Text>Log in to Household</Text>
+            <TextInput
+              style={{ borderRadius: 10 }}
+              mode="outlined"
+              label="Email"
+            />
+            <TextInput
+              style={{ borderRadius: 10 }}
+              mode="outlined"
+              label="Password"
+            />
+            <Button
+              style={{ marginTop: 10 }}
+              icon="login"
+              mode="contained"
+              onPress={() => console.log('Pressed')}
+            >
+              log in
+            </Button>
+            <View
+              style={{
+                flexDirection: 'row',
+                marginTop: 10,
+                justifyContent: 'center',
+              }}
+            >
+              <Text>Dont have an account? </Text>
+              <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+                <Text
+                  style={{ color: 'blue', textDecorationLine: 'underline' }}
+                >
+                  Sign-up
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
-}
+};
+
+export default SignIn;
