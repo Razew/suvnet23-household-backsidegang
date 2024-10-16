99e87a94b539dba026635e674d200368d130a51f
88jonhan <88jonhan@skola.boras.se>
2024-10-16 13:58:25 +0200

    changed app to only show statistics screen for ease of testing

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import RootStackNavigator from './navigators/RootStackNavigator';
+import StatisticsScreen from './screens/StatisticsScreen';
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <RootStackNavigator />
+      {/* <RootStackNavigator /> */}
+      <StatisticsScreen />
     </NavigationContainer>
   );
 }
