81f244138084205824b918923d4c817b362337ed
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2024-10-17 18:01:40 +0200

    useEffect loads household records. handleSubmit checks user input hiusehold and code against existing database records.
    Co-authored-by: Robin Lundahl <robinLundahl@users.noreply.github.com>

--- screens/CreateHouseholdScreen.tsx
+++ screens/CreateHouseholdScreen.tsx
@@ -1,17 +1,64 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
+import { Household } from '../types/types';
+import { supabase } from '../utils/supabase';
 
 export default function CreateHouseholdScreen() {
-  const [household, setHousehold] = useState('');
+  const [newHousehold, setNewHousehold] = useState('');
+  const [existingHouseholds, setExistingHouseholds] = useState<Household[]>([]);
   const [code, setCode] = useState('');
+
+  useEffect(() => {
+    getHouseholds();
+  }, []);
+
+  const getHouseholds = async () => {
+    try {
+      const { data: dbQueryResult, error } = await supabase
+        .from('household')
+        .select();
+
+      if (error) {
+        console.error(error.message);
+        throw error;
+      }
+
+      if (dbQueryResult && dbQueryResult.length > 0) {
+        console.log(`Total households in DB: ${dbQueryResult.length}`);
+        console.log(dbQueryResult);
+        setExistingHouseholds(dbQueryResult);
+      } else {
+        console.log('No household records found');
+      }
+    } catch (error) {
+      console.error('Error fetching households:', (error as Error).message);
+    }
+  };
+
+  const handleSubmit = async () => {
+    // check if our user household name & code match the db
+    const householdExists = existingHouseholds.some(
+      (h) =>
+        newHousehold.toLowerCase() === h.name.toLowerCase() &&
+        code.toLowerCase() === h.code.toLowerCase(),
+    );
+
+    if (householdExists) {
+      console.log(
+        `Household ${newHousehold} with code ${code} already exists in DB`,
+      );
+    } else {
+      console.log(`You have added ${newHousehold} with code ${code} to the DB`);
+    }
+  };
 
   return (
     <View style={style.container}>
       <TextInput
         label="Enter Household"
-        value={household}
-        onChangeText={(household) => setHousehold(household)}
+        value={newHousehold}
+        onChangeText={(household) => setNewHousehold(household)}
       />
       <TextInput
         label="Enter code"
@@ -20,7 +67,7 @@ export default function CreateHouseholdScreen() {
       />
       <Button
         mode="contained"
-        onPress={() => console.log(household, code)}
+        onPress={handleSubmit}
         style={style.button}
       >
         Create
