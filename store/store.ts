b82408766adbcec1a2cac3d45168e561cc009595
Markus <markus147@live.se>
2024-10-19 23:55:11 +0200

    Move thunk custom hook into separate file

--- store/store.ts
+++ store/store.ts
@@ -16,10 +16,4 @@ export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-// export const createAppAsyncThunk = createAsyncThunk.withTypes<{
-//   state: RootState;
-//   dispatch: AppDispatch;
-//   rejectValue: string;
-// }>();
-
 export default store;
