767a84644d9370f05a9b08aae19a2e3c12baa778
Markus <markus147@live.se>
2024-10-19 23:35:37 +0200

    Update to use env variables for db connection

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
 export const supabase = createClient(
-  'https://teokhvzdfbpemkugaoia.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlb2todnpkZmJwZW1rdWdhb2lhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxMDY2NjksImV4cCI6MjA0NDY4MjY2OX0.1CSAZsvNmlh7KL-Q_9IFESF9oiZ-Cxla93MTwImWNE0',
+  process.env.EXPO_PUBLIC_SUPABASE_URL || '',
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '',
   {
     auth: {
       storage: AsyncStorage,
