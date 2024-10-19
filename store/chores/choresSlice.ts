1391347a83d7dc45971e361364d1190f1e202cb4
Markus <markus147@live.se>
2024-10-19 22:30:18 +0200

    Add chores slice

---
+++ store/chores/choresSlice.ts
@@ -0,0 +1,71 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Chore } from '../../types/types';
+import { supabase } from '../../utils/supabase';
+import { createAppAsyncThunk } from '../hooks';
+import { RootState } from '../store';
+
+type ChoresState = {
+  allChores: Chore[];
+  errorMessage?: string;
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed';
+};
+
+const initialState: ChoresState = {
+  allChores: [],
+  loading: 'idle',
+};
+
+const choresSlice = createSlice({
+  name: 'chores',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchChores.pending, (state) => {
+      state.loading = 'pending';
+      state.errorMessage = undefined;
+    });
+    builder.addCase(
+      fetchChores.fulfilled,
+      (state, action: PayloadAction<Chore[]>) => {
+        state.allChores = action.payload;
+        state.loading = 'succeeded';
+      },
+    );
+    builder.addCase(fetchChores.rejected, (state, action) => {
+      state.errorMessage = action.payload;
+      state.loading = 'failed';
+    });
+  },
+});
+
+export const fetchChores = createAppAsyncThunk(
+  'chores/fetch-chores',
+  async (_, { rejectWithValue }) => {
+    console.log('Fetching chores...');
+    try {
+      const { data: fetchedChores, error } = await supabase
+        .from('chore')
+        .select('*');
+      console.log('Fetched Chores:', fetchedChores);
+
+      if (error) {
+        console.error('Supabase Error:', error);
+        return rejectWithValue(error.message);
+      }
+
+      if (!fetchedChores || fetchChores.length === 0) {
+        console.error('No chores found');
+        return rejectWithValue('No chores found');
+      }
+
+      return fetchedChores;
+    } catch (error) {
+      console.error(error);
+      return rejectWithValue('Error while fetching chores');
+    }
+  },
+);
+
+export const selectAllChores = (state: RootState) => state.chores.allChores;
+
+export default choresSlice.reducer;
