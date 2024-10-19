beba0664faf697103519b5598d22380fddd86c7e
Markus <markus147@live.se>
2024-10-19 23:52:11 +0200

    Add avatars slice

---
+++ store/avatars/avatarsSlice.ts
@@ -0,0 +1,71 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Avatar } from '../../types/types';
+import { supabase } from '../../utils/supabase';
+import { createAppAsyncThunk } from '../hooks';
+import { RootState } from '../store';
+
+type AvatarsState = {
+  allAvatars: Avatar[];
+  errorMessage?: string;
+  loading: 'idle' | 'pending' | 'succeeded' | 'failed';
+};
+
+const initialState: AvatarsState = {
+  allAvatars: [],
+  loading: 'idle',
+};
+
+const avatarsSlice = createSlice({
+  name: 'avatars',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAvatars.pending, (state) => {
+      state.loading = 'pending';
+      state.errorMessage = undefined;
+    });
+    builder.addCase(
+      fetchAvatars.fulfilled,
+      (state, action: PayloadAction<Avatar[]>) => {
+        state.allAvatars = action.payload;
+        state.loading = 'succeeded';
+      },
+    );
+    builder.addCase(fetchAvatars.rejected, (state, action) => {
+      state.errorMessage = action.payload;
+      state.loading = 'failed';
+    });
+  },
+});
+
+export const fetchAvatars = createAppAsyncThunk(
+  'avatars/fetch-avatars',
+  async (_, { rejectWithValue }) => {
+    console.log('Fetching avatars...');
+    try {
+      const { data: fetchedAvatars, error } = await supabase
+        .from('avatar')
+        .select('*');
+      console.log('Fetched Avatars:', fetchedAvatars);
+
+      if (error) {
+        console.error('Supabase Error:', error);
+        return rejectWithValue(error.message);
+      }
+
+      if (!fetchedAvatars || fetchedAvatars.length === 0) {
+        console.error('No avatars found');
+        return rejectWithValue('No avatars found');
+      }
+
+      return fetchedAvatars;
+    } catch (error) {
+      console.error(error);
+      return rejectWithValue('Error while fetching avatars');
+    }
+  },
+);
+
+export const selectAllAvatars = (state: RootState) => state.avatars.allAvatars;
+
+export default avatarsSlice.reducer;
--- store/store.ts
+++ store/store.ts
@@ -1,10 +1,12 @@
-import { configureStore, createAsyncThunk } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import avatarsReducer from './avatars/avatarsSlice';
 import householdsReducer from './households/slice';
 
 export const store = configureStore({
   reducer: {
     households: householdsReducer,
+    avatars: avatarsReducer,
   },
 });
 
@@ -14,10 +16,10 @@ export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-export const createAppAsyncThunk = createAsyncThunk.withTypes<{
-  state: RootState;
-  dispatch: AppDispatch;
-  rejectValue: string;
-}>();
+// export const createAppAsyncThunk = createAsyncThunk.withTypes<{
+//   state: RootState;
+//   dispatch: AppDispatch;
+//   rejectValue: string;
+// }>();
 
 export default store;
