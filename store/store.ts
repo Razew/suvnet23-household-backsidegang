a7e4d7fd8988a057f43282cc680e34082f2d3aad
Markus <markus147@live.se>
2024-10-21 13:38:09 +0200

    Merge commit '5bbdcde44446f5157262b451e888e189bf24dc46'

--- store/store.ts
+++ store/store.ts
@@ -1,10 +1,16 @@
-import { configureStore, createAsyncThunk } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { avatarsReducer } from './avatars/slice';
+import { choresToUsersReducer } from './choreToUser/slice';
 import householdsReducer from './households/slice';
+import { usersToHouseholdsReducer } from './userToHousehold/slice';
 
 export const store = configureStore({
   reducer: {
     households: householdsReducer,
+    choresToUsers: choresToUsersReducer,
+    usersToHouseholds: usersToHouseholdsReducer,
+    avatars: avatarsReducer,
   },
 });
 
@@ -13,11 +19,5 @@ export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-export const createAppAsyncThunk = createAsyncThunk.withTypes<{
-  state: RootState;
-  dispatch: AppDispatch;
-  rejectValue: string;
-}>();
 
 export default store;
