94d7aa2e653ea9a2da8578f2a5e7a40b4557ef64
Markus <markus147@live.se>
2024-10-21 13:43:07 +0200

    Refactor Redux store hooks

--- store/store.ts
+++ store/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { avatarsReducer } from './avatars/slice';
 import { choresToUsersReducer } from './choreToUser/slice';
 import householdsReducer from './households/slice';
@@ -16,8 +15,5 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
