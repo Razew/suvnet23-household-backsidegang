c8efef61a7c5752c153a51e89946e8b463c3b21d
Markus <Markus147@live.se>
2024-10-23 01:07:48 +0200

    Render ribbon on new chore, avatars, etc

--- components/ChoreCard.tsx
+++ components/ChoreCard.tsx
@@ -1,5 +1,10 @@
 import { Pressable, StyleSheet, View } from 'react-native';
 import { Surface, Text, useTheme } from 'react-native-paper';
+import {
+  selectDaysSinceLastCompleted,
+  selectUsersWithAvatarsWhoCompletedChoreToday,
+} from '../store/combinedSelectors';
+import { useAppSelector } from '../store/hooks';
 import { Chore } from '../types/types';
 
 type Props = {
@@ -8,10 +13,14 @@ type Props = {
 
 export default function ChoreCard({ chore }: Props) {
   const { colors } = useTheme();
-  const avatars = mockedChoreStatuses
-    .filter((status) => status.chore.id === chore.id)
-    .map((status) => status.user.avatar.image);
-  const daysSinceLastCompleted = 0; // Example value, replace with actual logic with appselector(?)
+  // const avatars = mockedChoreStatuses
+  //   .filter((status) => status.chore.id === chore.id)
+  //   .map((status) => status.user.avatar.image);
+  const profiles = useAppSelector(
+    selectUsersWithAvatarsWhoCompletedChoreToday(chore.id),
+  );
+  const daysSinceLastCompleted =
+    useAppSelector(selectDaysSinceLastCompleted(chore.id)) ?? -1;
 
   const getDaysContainerStyle = () => ({
     ...s.daysContainer,
@@ -29,6 +38,20 @@ export default function ChoreCard({ chore }: Props) {
         : colors.onSurfaceVariant,
   });
 
+  const ribbonContainerStyle = {
+    ...s.ribbonContainer,
+  };
+
+  const ribbonStyle = {
+    ...s.ribbon,
+    backgroundColor: colors.primary,
+  };
+
+  const ribbonTextStyle = {
+    ...s.ribbonText,
+    color: colors.onPrimary,
+  };
+
   return (
     <Pressable style={s.pressableContainer}>
       <Surface style={s.cardSurface}>
@@ -38,20 +61,42 @@ export default function ChoreCard({ chore }: Props) {
         >
           {chore.name}
         </Text>
-        {daysSinceLastCompleted > 0 ? (
-          <View style={getDaysContainerStyle()}>
-            <Text style={getDaysTextStyle()}>{daysSinceLastCompleted}</Text>
-          </View>
-        ) : (
-          avatars.slice(0, 3).map((avatar, index) => (
+        {daysSinceLastCompleted === 0 ? (
+          profiles.slice(0, 3).map((profile, index) => (
             <Text
               key={index}
               style={s.avatar}
             >
-              {avatar}
+              {profile.avatar?.emoji}
             </Text>
           ))
+        ) : // <Text>🦉</Text>
+        daysSinceLastCompleted > 0 ? (
+          <View style={getDaysContainerStyle()}>
+            <Text style={getDaysTextStyle()}>{daysSinceLastCompleted}</Text>
+          </View>
+        ) : (
+          <View style={ribbonContainerStyle}>
+            <View style={ribbonStyle}>
+              <Text style={ribbonTextStyle}>New</Text>
+            </View>
+          </View>
         )}
+        {/* {daysSinceLastCompleted > 0 ? (
+          <View style={getDaysContainerStyle()}>
+            <Text style={getDaysTextStyle()}>{daysSinceLastCompleted}</Text>
+          </View>
+        ) : (
+          <Text>Hi LOL</Text>
+          // avatars.slice(0, 3).map((avatar, index) => (
+          //   <Text
+          //     key={index}
+          //     style={s.avatar}
+          //   >
+          //     {avatar}
+          //   </Text>
+          // ))
+        )} */}
       </Surface>
     </Pressable>
   );
@@ -83,10 +128,39 @@ const s = StyleSheet.create({
   },
   choreTitle: {
     flex: 1,
-    fontSize: 22,
+    fontSize: 20,
     fontWeight: 'bold',
   },
   avatar: {
     fontSize: 22,
+  },
+  newLabelContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  newLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'green',
+  },
+  ribbonContainer: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    overflow: 'hidden',
+    width: 75,
+    height: 75,
+  },
+  ribbon: {
+    position: 'absolute',
+    top: 10,
+    right: -20,
+    transform: [{ rotate: '45deg' }],
+    padding: 5,
+    width: 100,
+  },
+  ribbonText: {
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
--- screens/HouseholdScreen.tsx
+++ screens/HouseholdScreen.tsx
@@ -1,18 +1,18 @@
-import { useEffect } from 'react';
 import { ScrollView, StyleSheet } from 'react-native';
 import { Text } from 'react-native-paper';
 import ChoreCard from '../components/ChoreCard';
-import { fetchChores, selectChores } from '../store/chores/slice';
-import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { selectActiveChoresCurrentHousehold } from '../store/combinedSelectors';
+import { useAppSelector } from '../store/hooks';
 import { large } from '../themes/styles';
 
 export default function HouseholdScreen() {
-  const chores = useAppSelector(selectChores);
-  const dispatch = useAppDispatch();
+  const chores = useAppSelector(selectActiveChoresCurrentHousehold);
+  // const chores = useAppSelector(selectChores);
+  // const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(fetchChores());
-  }, []);
+  // useEffect(() => {
+  //   dispatch(fetchChores());
+  // }, []);
 
   return (
     <ScrollView contentContainerStyle={s.root}>
