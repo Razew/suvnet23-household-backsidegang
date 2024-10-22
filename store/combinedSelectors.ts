3357ee4ee015318451fb1e3e8a251c08f8cba570
Markus <Markus147@live.se>
2024-10-23 01:08:31 +0200

    More selectors

--- store/combinedSelectors.ts
+++ store/combinedSelectors.ts
@@ -1,6 +1,7 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { selectAvatars } from './avatars/slice';
 import { selectChores } from './chores/slice';
+import { selectChoresToUsers } from './choreToUser/slice';
 import { selectCurrentHousehold } from './households/slice';
 import { selectUsersToHouseholds } from './userToHousehold/slice';
 
@@ -21,7 +22,73 @@ export const selectActiveChoresCurrentHousehold = createSelector(
   },
 );
 
-// export const selectChoreRecords;
+// export const selectActiveCompletedChoresCurrentHousehold = createSelector([selectActiveChoresCurrentHousehold,],
+//   (chores) => {
+//     return chores.filter((chore) => chore)
+//   }
+// )
+
+// CHORES TO USERS, AKA CHORE RECORDS
+export const selectChoreRecords = (choreId: number) =>
+  createSelector([selectChoresToUsers], (choreRecords) => {
+    return choreRecords.filter(
+      (choreRecord) => choreRecord.chore_id === choreId,
+    );
+  });
+
+export const selectChoresCompletedToday = (choreId: number) =>
+  createSelector([selectChoreRecords(choreId)], (choreRecords) => {
+    const today = new Date().toDateString();
+    return choreRecords.filter(
+      (choreRecord) =>
+        choreRecord.is_completed &&
+        new Date(choreRecord.done_date).toDateString() === today,
+    );
+  });
+
+export const selectUsersWithAvatarsWhoCompletedChoreToday = (choreId: number) =>
+  createSelector(
+    [
+      selectChoresCompletedToday(choreId),
+      selectUsersWithAvatarsCurrentHousehold,
+    ],
+    (choreRecords, users) => {
+      const userIds = choreRecords.map((record) => record.user_id);
+      return users.filter((user) => userIds.includes(user.user_id));
+    },
+  );
+
+export const selectSortedChoreRecordsByCompletionDate = (choreId: number) =>
+  createSelector([selectChoreRecords(choreId)], (choreRecords) => {
+    const completedChores = choreRecords.filter((chore) => chore.is_completed);
+
+    return completedChores.sort(
+      (a, b) =>
+        new Date(a.done_date).getTime() - new Date(b.done_date).getTime(),
+    );
+  });
+
+export const selectDaysSinceLastCompleted = (choreId: number) =>
+  createSelector(
+    [selectSortedChoreRecordsByCompletionDate(choreId)],
+    (choreRecords) => {
+      const validChoreRecords = choreRecords.filter(
+        (chore) => chore.done_date !== null,
+      );
+
+      if (validChoreRecords.length === 0) return null;
+
+      const lastCompletedDate = new Date(choreRecords[0].done_date);
+      const today = new Date();
+      const timeDiff = today.getTime() - lastCompletedDate.getTime();
+      const daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
+
+      return daysDiff;
+    },
+  );
+
+// export const selectChoreRecordsForUser = (userId: number) =>
+//   createSelector([selectChoresToUserById(userId), selectActiveChoresCurrentHousehold], ());
 
 // USERS
 export const selectUsersCurrentHousehold = createSelector(
