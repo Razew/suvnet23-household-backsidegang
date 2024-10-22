699f90f568985249de617c331aa480cebb7680dc
Markus <Markus147@live.se>
2024-10-22 18:17:57 +0200

    Add selectors for active chores and users with avatars in the current household

--- store/combinedSelectors.ts
+++ store/combinedSelectors.ts
@@ -1,12 +1,40 @@
 import { createSelector } from '@reduxjs/toolkit';
+import { selectAvatars } from './avatars/slice';
 import { selectChores } from './chores/slice';
 import { selectCurrentHousehold } from './households/slice';
+import { selectUsersToHouseholds } from './userToHousehold/slice';
 
+// CHORES
 export const selectChoresCurrentHousehold = createSelector(
   [selectChores, selectCurrentHousehold],
   (chores, currentHousehold) => {
     return chores.filter(
       (chore) => chore.household_id === currentHousehold?.id,
     );
+  },
+);
+
+export const selectActiveChoresCurrentHousehold = createSelector(
+  [selectChoresCurrentHousehold],
+  (chores) => {
+    return chores.filter((chore) => chore.is_active && !chore.is_archived);
+  },
+);
+
+// USERS
+export const selectUsersCurrentHousehold = createSelector(
+  [selectUsersToHouseholds, selectCurrentHousehold],
+  (users, currentHousehold) => {
+    return users.filter((user) => user.household_id === currentHousehold?.id);
+  },
+);
+
+export const selectUsersWithAvatarsCurrentHousehold = createSelector(
+  [selectUsersCurrentHousehold, selectAvatars],
+  (users, avatars) => {
+    return users.map((user) => ({
+      ...user,
+      avatar: avatars.find((avatar) => avatar.id === user.avatar_id),
+    }));
   },
 );
