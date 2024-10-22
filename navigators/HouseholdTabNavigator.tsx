89ec16e03d8b01f1399555091c0ea372307aa3a6
88jonhan <88jonhan@skola.boras.se>
2024-10-22 10:18:15 +0200

    commented this out because its not needed with current solution

--- navigators/HouseholdTabNavigator.tsx
+++ navigators/HouseholdTabNavigator.tsx
@@ -1,27 +1,64 @@
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import HouseholdScreen from '../screens/HouseholdScreen';
-import StatisticsScreen from '../screens/StatisticsScreen';
+// import React, { useCallback, useState } from 'react';
+// import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+// import { useFocusEffect, useRoute } from '@react-navigation/native';
+// import HouseholdScreen from '../screens/HouseholdScreen';
+// import LastMonthScreen from '../screens/statistics/LastMonthScreen';
+// import LastWeekScreen from '../screens/statistics/LastWeekScreen';
+// import ThisWeekScreen from '../screens/statistics/ThisWeekScreen';
+// import HouseholdHeader from '../screens/HouseholdHeader';
+// import { set } from 'react-hook-form';
 
-export type HouseholdTabParamList = {
-  Household: undefined;
-  Statistics: undefined;
-  // Schedule: undefined;
-  // Admin: undefined; // This is just an example
-};
+// import { LogBox } from 'react-native';
 
-const Tab = createMaterialTopTabNavigator<HouseholdTabParamList>();
+// LogBox.ignoreLogs([
+//   'Non-serializable values were found in the navigation state',
+// ]);
 
-export default function HouseholdTabNavigator() {
-  return (
-    <Tab.Navigator initialRouteName="Household">
-      <Tab.Screen
-        name="Household"
-        component={HouseholdScreen}
-      />
-      <Tab.Screen
-        name="Statistics"
-        component={StatisticsScreen}
-      />
-    </Tab.Navigator>
-  );
-}
+// export type HouseholdTabParamList = {
+//   Household: { updateTitle: (title: string) => void };
+
+// };
+
+// const Tab = createMaterialTopTabNavigator<HouseholdTabParamList>();
+// export default function HouseholdTabNavigator() {
+//   const [title, setTitle] = useState('Household');
+//   const updateTitle = (screenTitle: string): void => {
+//     setTitle(screenTitle);
+//   };
+//   return (
+//     <Tab.Navigator
+//       initialRouteName={'Household'}
+//       tabBar={({ navigation }: { navigation: any }) => (
+//         <HouseholdHeader
+//           navigation={navigation}
+//           title={title}
+//         />
+//       )}
+//     >
+//       <Tab.Screen
+//         name="Household"
+//         component={HouseholdScreen}
+//         options={{ title: 'Daily' }}
+//         initialParams={{ updateTitle }}
+//       />
+//       <Tab.Screen
+//         name="ThisWeekStatistics"
+//         component={ThisWeekScreen}
+//         options={{ title: 'This Week' }}
+//         initialParams={{ updateTitle }}
+//       />
+//       <Tab.Screen
+//         name="LastWeekStatistics"
+//         component={LastWeekScreen}
+//         options={{ title: 'Last Week' }}
+//         initialParams={{ updateTitle }}
+//       />
+//       <Tab.Screen
+//         name="LastMonthStatistics"
+//         component={LastMonthScreen}
+//         options={{ title: 'Last Month' }}
+//         initialParams={{ updateTitle }}
+//       />
+//     </Tab.Navigator>
+//   );
+// }
