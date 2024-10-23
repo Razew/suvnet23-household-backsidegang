6b15988c7d30317f6ff5290c9f2b13e7cc31e287
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2024-10-23 18:29:47 +0200

    Updated migrations

---
+++ components/ChangeHousehold.tsx
@@ -0,0 +1,17 @@
+import { View } from 'react-native';
+import { Text } from 'react-native-paper';
+
+// Need current logged in user
+// Need last selected household
+// To list all available households
+
+const loggedInUserid: number = 4;
+const lastSelectedHousehold: number = 1;
+
+export default function ChangeHousehold() {
+  return (
+    <View>
+      <Text>Change Household</Text>
+    </View>
+  );
+}
