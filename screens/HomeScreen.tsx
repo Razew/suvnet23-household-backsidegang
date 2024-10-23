381f8d33ef4c6d518377591cc73b6a62d4d53248
88jonhan <88jonhan@skola.boras.se>
2024-10-23 20:05:08 +0200

    shows household cards based on users households

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,31 +1,67 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { StyleSheet, View } from 'react-native';
-import { Button, Text } from 'react-native-paper';
+import { Button, Surface, Text } from 'react-native-paper';
 import { HomeStackParamList } from '../navigators/HomeStackNavigator';
 import { container, large } from '../themes/styles';
+import { selectAllHouseholds } from '../store/households/slice';
+import { selectUsersToHouseholds } from '../store/userToHousehold/slice';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { User_To_Household } from '../types/types';
+import { selectLoggedInUser } from '../store/Auth/slice';
+import HouseholdCard from '../components/HouseholdCard';
 type Props = NativeStackScreenProps<HomeStackParamList, 'Home'>;
 
 export default function HomeScreen({ navigation }: Props) {
-  return (
+  const loggedInUser = useAppSelector(selectLoggedInUser);
+  if (!loggedInUser) {
     <View style={container}>
-      <Text style={large}>Home screen</Text>
-      <View style={s.tempHouseholdContainer}>
-        <Text style={s.tempHouseholdName}>CodeDiddy Household</Text>
+      <Text style={large}>Not logged in</Text>
+    </View>;
+  }
+
+  const allHouseholds = useAppSelector(selectAllHouseholds);
+  const allUserToHouseholds: User_To_Household[] = useAppSelector(
+    selectUsersToHouseholds,
+  );
+  const userHouseholds: User_To_Household[] = allUserToHouseholds.filter(
+    (userToHousehold) => userToHousehold.user_id === loggedInUser?.id,
+  );
+
+  const profileAndHouseholds = userHouseholds.map((userHousehold) => {
+    const household = allHouseholds.find(
+      (household) => household.id === userHousehold.household_id,
+    )!;
+    return { household, profile: userHousehold };
+  });
+
+  return (
+    <Surface style={container}>
+      <Surface
+        style={s.cardContainer}
+        elevation={0}
+      >
+        {userHouseholds.length > 0 ? (
+          profileAndHouseholds.map((profileAndHousehold) => (
+            <HouseholdCard
+              key={profileAndHousehold.household.id}
+              household={profileAndHousehold.household}
+              profile={profileAndHousehold.profile}
+              navigation={navigation}
+            />
+          ))
+        ) : (
+          <Text>No households found</Text>
+        )}
+      </Surface>
+      <Surface elevation={0}>
         <Button
           mode="elevated"
-          onPress={() => navigation.replace('HouseholdScreen')}
+          onPress={() => navigation.replace('CreateHousehold')}
         >
-          Enter
+          Create Household
         </Button>
-      </View>
-      <Button mode="elevated">Another Button</Button>
-      <Button
-        mode="elevated"
-        onPress={() => navigation.replace('CreateHousehold')}
-      >
-        Create Household
-      </Button>
-    </View>
+      </Surface>
+    </Surface>
   );
 }
 
@@ -35,5 +71,11 @@ const s = StyleSheet.create({
   },
   tempHouseholdName: {
     fontSize: 18,
+  },
+  cardContainer: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: '100%',
+    padding: 10,
   },
 });
