e3d87dd1455bef000c354645e4ff10fcd782b8cb
88jonhan <88jonhan@skola.boras.se>
2024-10-23 13:29:44 +0200

    implemented getThisWeek(), getLastWeek(), getLastMonth()

--- utils/statistics.ts
+++ utils/statistics.ts
@@ -1,3 +1,22 @@
+export const getThisWeekDates = (): string[] => {
+  const dates = [];
+  const today = new Date();
+  const currentDayOfWeek = today.getDay();
+
+  const daysSinceThisMonday = currentDayOfWeek === 0 ? 6 : currentDayOfWeek - 1;
+
+  const thisMonday = new Date(today);
+  thisMonday.setDate(today.getDate() - daysSinceThisMonday);
+
+  for (let i = 0; i < 7; i++) {
+    const date = new Date(thisMonday);
+    date.setDate(thisMonday.getDate() + i);
+    dates.push(date.toISOString().split('T')[0]);
+  }
+
+  return dates;
+};
+
 export const getLastWeekDates = (): string[] => {
   const dates = [];
   const today = new Date();
@@ -16,13 +35,32 @@ export const getLastWeekDates = (): string[] => {
   return dates;
 };
 
-export const getMonthDates = (monthsAgo: number): string[] => {
+export const getLastMonthDates = (): string[] => {
   const dates = [];
-  const now = new Date();
-  const start = new Date(now.getFullYear(), now.getMonth() - monthsAgo, 1);
-  const end = new Date(now.getFullYear(), now.getMonth() - monthsAgo + 1, 0);
-  for (let d = start; d <= end; d.setDate(d.getDate() + 1)) {
-    dates.push(new Date(d).toISOString().split('T')[0]);
+  const today = new Date();
+
+  const firstDayOfCurrentMonth = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    1,
+  );
+
+  const lastDayOfLastMonth = new Date(firstDayOfCurrentMonth);
+
+  const firstDayOfLastMonth = new Date(
+    lastDayOfLastMonth.getFullYear(),
+    lastDayOfLastMonth.getMonth(),
+    1,
+  );
+  lastDayOfLastMonth.setDate(firstDayOfLastMonth.getDate() - 1);
+
+  for (
+    let date = firstDayOfLastMonth;
+    date <= lastDayOfLastMonth;
+    date.setDate(date.getDate() + 1)
+  ) {
+    dates.push(new Date(date).toISOString().split('T')[0]);
   }
+
   return dates;
 };
