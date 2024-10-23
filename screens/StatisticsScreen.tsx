7036886e921ed1533bbeb9241479f992a88e16cc
88jonhan <88jonhan@skola.boras.se>
2024-10-23 12:36:56 +0200

    changed how statistics work again

--- screens/StatisticsScreen.tsx
+++ screens/StatisticsScreen.tsx
@@ -6,128 +6,179 @@ import {
   Dimensions,
   ActivityIndicator,
 } from 'react-native';
-import {
-  getChoresData,
-  users,
-  households,
-  chores,
-  completedChores,
-} from '../mocked';
 import CustomPieChart from '../components/CustomPieChart';
-import { Surface } from 'react-native-paper';
-import { container } from '../themes/styles';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
-import { selectLoggedInUser } from '../store/Auth/slice';
-import {
-  fetchHouseholds,
-  selectAllHouseholds,
-} from '../store/households/slice';
-import { fetchChores, selectAllChores } from '../store/chores/slice';
 import { fetchAvatars, selectAvatars } from '../store/avatars/slice';
+import { fetchChores, selectChores } from '../store/chores/slice';
 import {
   fetchChoresToUsers,
   selectChoresToUsers,
 } from '../store/choreToUser/slice';
 import {
+  fetchHouseholds,
+  selectAllHouseholds,
+} from '../store/households/slice';
+import {
   fetchUsersToHouseholds,
   selectUsersToHouseholds,
 } from '../store/userToHousehold/slice';
+import {
+  User,
+  PieDataItem,
+  Chore,
+  Household,
+  Avatar,
+  User_To_Household,
+  Chore_To_User,
+} from '../types/types';
 
 interface StatisticsScreenProps {
   timespan: string[];
 }
 
-interface PieDataItem {
-  value: number;
-  color: string;
-  emoji: string;
-}
-
-interface ChoreDataMap {
-  [key: number]: PieDataItem[];
-}
-
-const household = households[0];
 const screenWidth = Dimensions.get('window').width;
-const smallChartRadius = screenWidth * 0.14;
+const smallChartRadius = screenWidth * 0.15;
 
 export default function StatisticsScreen({ timespan }: StatisticsScreenProps) {
   const dispatch = useAppDispatch();
 
+  // Fetch all necessary data
   useEffect(() => {
+    dispatch(fetchHouseholds());
     dispatch(fetchChores());
     dispatch(fetchAvatars());
     dispatch(fetchChoresToUsers());
-    dispatch(fetchHouseholds());
     dispatch(fetchUsersToHouseholds());
-  }, []);
+  }, [dispatch]);
 
-  const loggedInUser = useAppSelector(selectLoggedInUser);
   const allHouseholds = useAppSelector(selectAllHouseholds);
+  const allChores = useAppSelector(selectChores);
   const allAvatars = useAppSelector(selectAvatars);
-  const allChoreToUser = useAppSelector(selectChoresToUsers);
-  const allUserToHousehold = useAppSelector(selectUsersToHouseholds);
-  // const allChores = useAppSelector(selectAllChores);
+  const allChoreToUsers = useAppSelector(selectChoresToUsers);
+  const allUserToHouseholds = useAppSelector(selectUsersToHouseholds);
 
-  console.log('Logged in user: ', loggedInUser);
-  console.log('selectAllHouseholds: ', allHouseholds);
-  console.log('allAvatars: ', allAvatars);
-  console.log('allChoreToUser: ', allChoreToUser);
-  console.log('allUserToHousehold: ', allUserToHousehold);
+  const loggedInUser: User = { id: 53, username: 'Anna', password: '1234' };
 
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState<PieDataItem[]>([]);
-  const [choreData, setChoreData] = useState<ChoreDataMap>({});
+  const [chartData, setChartData] = useState<PieDataItem[]>([]);
+  const [topChoresData, setTopChoresData] = useState<
+    { name: string; chartData: PieDataItem[] }[]
+  >([]);
+  const userHouseholds = allUserToHouseholds.filter(
+    (userToHousehold) => userToHousehold.user_id === loggedInUser.id,
+  );
+  let householdChores: Chore[] = [];
+  let currentHouseholdUsers: User_To_Household[] = [];
+  let completedChores: Chore_To_User[] = [];
+
+  const household = userHouseholds[0];
 
   useEffect(() => {
     const fetchData = async () => {
+      household
+        ? console.log('household found for the user')
+        : console.log('No household found for the user');
+
       setLoading(true);
-      const choresData = getChoresData(timespan, household.id);
-      const filteredData = choresData.filter((chore) => chore.completed > 0); // Filter out users with zero completed chores
-      const totalData = filteredData.map((chore) => {
-        const user = users.find((user) => user.name === chore.user);
-        return {
-          value: chore.completed,
-          color: user?.color || '#000000',
-          emoji: user?.emoji || '',
-        };
+
+      // Filter household-specific chores and users
+      if (allChores.length < 1) {
+        console.log('No chores found');
+      } else {
+        householdChores = allChores.filter((chore) => {
+          return household
+            ? chore.household_id === household.household_id
+            : false;
+        });
+      }
+      if (allUserToHouseholds.length < 1) {
+        console.log('No userToHouseholds found');
+      } else {
+        currentHouseholdUsers = allUserToHouseholds.filter(
+          (userToHousehold) =>
+            userToHousehold.household_id === household.household_id,
+        );
+      }
+
+      if (householdChores.length < 1) {
+        console.log('No chores found for the household');
+      } else {
+        completedChores = allChoreToUsers.filter(
+          (choreToUser) =>
+            choreToUser.is_completed &&
+            householdChores.some(
+              (chore) => chore.id === choreToUser.chore_id,
+            ) &&
+            timespan.includes(choreToUser.done_date.slice(0, 10)),
+        );
+      }
+      // Aggregated data for all chores
+      const aggregatedChartData: PieDataItem[] = currentHouseholdUsers
+        .map((userToHousehold) => {
+          const userCompletedChores = completedChores.filter(
+            (choreToUser) => choreToUser.user_id === userToHousehold.user_id,
+          ).length;
+          const avatar = allAvatars.find(
+            (avatar) => avatar.id === userToHousehold.avatar_id,
+          );
+
+          return {
+            value: userCompletedChores,
+            color: avatar?.colour_code || '#000000',
+            emoji: avatar?.emoji || '',
+          };
+        })
+        .filter((item) => item.value > 0); // Exclude users with zero completed chores
+
+      // Calculate top 6 most completed chores
+      const choreCounts: { [key: number]: number } = {};
+      completedChores.forEach((choreToUser) => {
+        if (choreCounts[choreToUser.chore_id] === undefined) {
+          choreCounts[choreToUser.chore_id] = 1;
+        } else {
+          choreCounts[choreToUser.chore_id]++;
+        }
       });
 
-      const choreDataMap: ChoreDataMap = {};
-      chores
-        .filter((chore) => chore.householdId === household.id)
-        .forEach((chore) => {
-          const data = getChoreData(chore.id);
-          choreDataMap[chore.id] = data;
-        });
+      const topChores = Object.entries(choreCounts)
+        // .filter(([_, count]) => count > 0)
+        .sort((a, b) => b[1] - a[1])
+        .map(([choreId]) => parseInt(choreId));
+      console.log(topChores);
+      // Data for each top chore
+      const topChoresWithData = topChores.map((choreId) => {
+        const choreData: PieDataItem[] = currentHouseholdUsers
+          .map((userToHousehold) => {
+            const userCompletedChores = completedChores.filter(
+              (choreToUser) =>
+                choreToUser.user_id === userToHousehold.user_id &&
+                choreToUser.chore_id === choreId,
+            ).length;
+            const avatar = allAvatars.find(
+              (avatar) => avatar.id === userToHousehold.avatar_id,
+            );
 
-      setData(totalData);
-      setChoreData(choreDataMap);
+            return {
+              value: userCompletedChores,
+              color: avatar?.colour_code || '#000000',
+              emoji: avatar?.emoji || '',
+            };
+          })
+          .filter((item) => item.value > 0);
+
+        const choreName =
+          allChores.find((chore) => chore.id === choreId)?.name ||
+          'Unknown Chore';
+
+        return { name: choreName, chartData: choreData };
+      });
+
+      setChartData(aggregatedChartData);
+      setTopChoresData(topChoresWithData);
       setLoading(false);
     };
-
     fetchData();
-  }, [timespan]);
-
-  const getChoreData = (choreId: number) => {
-    const choreData = completedChores.filter(
-      (chore) => chore.choreId === choreId && timespan.includes(chore.date),
-    );
-    const userChoreCounts = users
-      .filter((user) => user.householdId === household.id)
-      .map((user) => {
-        const completed = choreData.filter(
-          (chore) => chore.userId === user.id,
-        ).length;
-        return {
-          value: completed,
-          color: user?.color || '#000000',
-          emoji: user?.emoji || '',
-        };
-      })
-      .filter((chore) => chore.value > 0);
-    return userChoreCounts;
-  };
+  }, [timespan, allChores, allChoreToUsers, allUserToHouseholds, allAvatars]);
 
   if (loading) {
     return (
@@ -142,49 +193,39 @@ export default function StatisticsScreen({ timespan }: StatisticsScreenProps) {
   }
 
   return (
-    <Surface
-      style={container}
-      elevation={0}
-    >
-      <Surface
-        style={styles.totalContainer}
-        elevation={0}
-      >
+    <View style={[styles.container, { flexDirection: 'column' }]}>
+      {/* Pie chart for aggregated data (all chores) */}
+      <View style={styles.totalContainer}>
         <CustomPieChart
-          data={data}
+          data={chartData}
           radius={150}
         />
-      </Surface>
-      <Surface
-        style={styles.choresContainer}
-        elevation={0}
-      >
-        {chores
-          .filter((chore) => chore.householdId === household.id)
-          .map((chore) => (
-            <Surface
-              key={chore.id}
-              style={styles.choreChart}
-              elevation={0}
-            >
-              <CustomPieChart
-                data={choreData[chore.id]}
-                radius={smallChartRadius}
-              />
-              <Surface
-                style={styles.choreTextContainer}
-                elevation={0}
-              >
-                <Text style={styles.choreText}>{chore.name}</Text>
-              </Surface>
-            </Surface>
-          ))}
-      </Surface>
-    </Surface>
+      </View>
+
+      {/* Pie charts for individual top chores */}
+      <View style={styles.choresContainer}>
+        {topChoresData.map((chore, index) => (
+          <View
+            key={index}
+            style={styles.choreChart}
+          >
+            <CustomPieChart
+              data={chore.chartData}
+              radius={50}
+            />
+            <Text style={styles.choreText}>{chore.name}</Text>
+          </View>
+        ))}
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
   loadingContainer: {
     flex: 1,
     justifyContent: 'center',
@@ -202,13 +243,9 @@ const styles = StyleSheet.create({
     margin: 10,
     alignItems: 'center',
   },
-  choreTextContainer: {
-    width: screenWidth * 0.25,
-  },
   choreText: {
-    marginTop: 5,
-    fontSize: 16,
+    marginVertical: 5,
+    fontSize: 14,
     fontWeight: 'bold',
-    textAlign: 'center',
   },
 });
