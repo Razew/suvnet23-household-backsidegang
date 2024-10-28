37c5640930ffc6218f63e39ebd93c34b3b734979
Markus <Markus147@live.se>
2024-10-28 01:28:18 +0100

    Style buttons to give illusion of more depth

--- components/ButtonGroup.tsx
+++ components/ButtonGroup.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, View } from 'react-native';
-import { Button, useTheme } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+import { Button, Surface, useTheme } from 'react-native-paper';
 
 export type ButtonGroupProps = {
   buttons: {
@@ -15,21 +15,28 @@ export default function ButtonGroup({ buttons }: ButtonGroupProps) {
   const { colors } = useTheme();
 
   return (
-    <View style={[s.buttonRow, { backgroundColor: colors.elevation.level2 }]}>
+    <Surface
+      style={[
+        s.buttonRow,
+        { paddingTop: 0.75 },
+        // { backgroundColor: colors.elevation.level2 },
+      ]}
+      elevation={5}
+    >
       {buttons.map((button, index) => (
         <Button
           key={index}
           icon={button.icon}
           mode={button.mode ?? 'elevated'}
           elevation={5}
-          style={s.button}
+          style={[s.button, { backgroundColor: colors.elevation.level2 }]}
           onPress={button.onPress}
           labelStyle={{ fontSize: button.labelSize ?? 16 }}
         >
           {button.label}
         </Button>
       ))}
-    </View>
+    </Surface>
   );
 }
 
@@ -41,6 +48,6 @@ const s = StyleSheet.create({
   },
   buttonRow: {
     flexDirection: 'row',
-    gap: 1,
+    // gap: 1,
   },
 });
