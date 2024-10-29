03ccc62447f91bad3b1dd002eeecdc15dcacdc45
Alex <alexjoelaid97@gmail.com>
2024-10-29 10:37:43 +0100

    new db, prev was full

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -3,8 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
 export const supabase = createClient(
-  'https://cekznfkclcpyfxvzipqb.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNla3puZmtjbGNweWZ4dnppcHFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkyMzM0NTIsImV4cCI6MjA0NDgwOTQ1Mn0.puPkuPS1TgNLIH1ub_ZDoj1YMBDVBvkjllhUqUv4K5A',
+"https://menzmxvjpkublxsbxwhb.supabase.co",
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lbnpteHZqcGt1Ymx4c2J4d2hiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAxOTIxMTIsImV4cCI6MjA0NTc2ODExMn0.au5SBhJbdP9gIdJQ8bRqKtDs6hLJieN5FjBQvOIOlRc",
+
+  // 'https://cekznfkclcpyfxvzipqb.supabase.co',
+  // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNla3puZmtjbGNweWZ4dnppcHFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkyMzM0NTIsImV4cCI6MjA0NDgwOTQ1Mn0.puPkuPS1TgNLIH1ub_ZDoj1YMBDVBvkjllhUqUv4K5A',
   {
     auth: {
       storage: AsyncStorage,
